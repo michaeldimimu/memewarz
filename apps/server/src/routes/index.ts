b4fcef96ee8fa3570e2c142e2e1324da1322ab45
Bemdoo Maor <bemdoo.maor1@gmail.com>
2026-08-22 10:52:56 +0100

    fix: update router type annotation for express.Router in index.ts

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,7 +1,7 @@
-import { Router } from 'express';
+import express from 'express';
 import healthRouter from './health';
 
-const router = Router();
+const router: express.Router = express.Router();
 
 router.use('/health', healthRouter);
 
